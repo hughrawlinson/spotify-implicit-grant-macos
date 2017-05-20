6d94265902e3b575067cc590540211bee81bf1b9
Hugh Rawlinson <hugh@spotify.com>
2017-05-21 00:03:00 +0200

    Initial Commit

---
+++ implicit-grant/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  implicit-grant
+//
+//  Created by Hugh Rawlinson on 2017-05-21.
+//  Copyright © 2017 Hugh Rawlinson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ implicit-grant/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  implicit-grant
+//
+//  Created by Hugh Rawlinson on 2017-05-21.
+//  Copyright © 2017 Hugh Rawlinson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
